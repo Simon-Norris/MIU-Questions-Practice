09d53c808f6f79028ad347397c05bf6e2e4e62d3
simon Norris <sumanneupane19982055@gmail.com>
2022-07-20 12:22:14 +0545

    find length of  longest substring without repeating characters using Hash Map

--- src/com/learning/java/Main.java
+++ src/com/learning/java/Main.java
@@ -5,8 +5,11 @@ public class Main
 {
 
     public static void main(String[] args) {
+
+        // find hte longest substring without repeating characters
         String[] toCheck = {"abcabacbb","bbbbb","pwwkew"};
         System.out.println(SlidingWindowProblems.longestSubstring(toCheck[0]));
+        System.out.println(SlidingWindowProblems.longestSubstringWithHash(toCheck[2]));
     }
 }
 
--- src/com/learning/java/SlidingWindowProblems.java
+++ src/com/learning/java/SlidingWindowProblems.java
@@ -20,4 +20,21 @@ class SlidingWindowProblems {
         }
         return maxLength;
     }
+    public static int longestSubstringWithHash(String s) {
+        if (s.isEmpty()){
+            return 0;
+        } else if (s.length() == 1) {
+            return 1;
+        }
+        HashMap<Character, Integer> seen = new HashMap<>();
+        int maxLength =0, start=0;
+        for (int end=0; end < s.length(); end++){
+            if (seen.containsKey(s.charAt(end))) {
+                start = Math.max(start, seen.get(s.charAt(end))+1);
+            }
+            seen.put(s.charAt(end), end);
+            maxLength = Math.max(maxLength, end-start+1);
+        }
+        return maxLength;
+    }
 }
