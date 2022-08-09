7e52586d5864ecb235b2f7c7ed8298c0cbfeaead
simon Norris <sumanneupane19982055@gmail.com>
2022-08-09 13:55:42 +0545

    m-n sequenced array

--- src/com/learning/java/Main.java
+++ src/com/learning/java/Main.java
@@ -5,7 +5,11 @@ public class Main
 {
 
     public static void main(String[] args) {
-        System.out.print(Solution.checkConcatenatedSum(198,2));
+        //System.out.print(Solution.checkConcatenatedSum(198,2));
+        int[] arr = {2, 2, 3, 4, 4, 4, 5,5,5};
+        int m = 2;
+        int n = 5;
+        System.out.println(Solution.isSequencedArray(arr, m, n));
     }
 }
 
--- src/com/learning/java/Solution.java
+++ src/com/learning/java/Solution.java
@@ -30,4 +30,49 @@ public class Solution {
         }
         return 1;
     }
+
+    /**
+     * Define an m-n sequenced array to be an array that contains one or more occurrences of all the integers between m and n inclusive.
+     * Furthermore, the array must be in ascending order and contain only those integers. For example, {2, 2, 3, 4, 4, 4, 5} is a 2-5 sequenced
+     * array. The array {2, 2, 3, 5, 5, 5} is not a 2-5 sequenced array because it is missing a 4. The array {0, 2, 2, 3, 3} is not a 2-3 sequenced
+     * array because the 0 is out of range. And {1,1, 3, 2, 2, 4} is not a 1-4 sequenced array because it is not in ascending order.
+     * Write a method named isSequencedArray that returns 1 if its argument is a m-n sequenced array, otherwise it returns 0.
+     * If you are writing in Java or C# the function signature is
+     * int isSequencedArray(int[ ] a, int m, int n)
+     * If you are writing in C or C++ the function signature is
+     * int isSequencedArray(int a[ ], int len, int m, int n) where len is the number of elements in the array a.
+     * You may assume that m<=n.
+     * **/
+   public static int isSequencedArray(int[] a, int m, int n) {
+        int start = a[0];
+        int end = a[a.length-1];
+        if ( start != m  || end != n) return 0;
+        for (int i=0; i+1 < a.length; i++) {
+            if (a[i] > a[i+1]) return 0;
+        }
+        int arrLength = removeDuplicates(a);
+        for (int i=0; i<arrLength; i++){
+            if (m != a[i]) {
+                return 0;
+            }
+            m++;
+        }
+        return 1;
+   }
+   /**
+    * Removes all the duplicate elements in an array.
+    * Returns the length of array upto which the elements are unique.
+    * Array should be sorted before invoking this function.
+    * **/
+   private static int removeDuplicates(int[] arr) {
+       int slowPointer =0 , fastPointer = 1;
+       while (fastPointer < arr.length) {
+           if (arr[slowPointer] != arr[fastPointer]) {
+               arr[slowPointer+1] = arr[fastPointer];
+               slowPointer++;
+           }
+           fastPointer++;
+       }
+       return slowPointer+1;
+   }
 }
