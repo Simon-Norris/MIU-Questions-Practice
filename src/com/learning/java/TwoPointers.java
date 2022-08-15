2843bd92bc589d165eaf0c670a60164bc1ccfa39
simon Norris <sumanneupane19982055@gmail.com>
2022-08-16 05:16:21 +0545

    search in rotated array using O(log n)

--- src/com/learning/java/TwoPointers.java
+++ src/com/learning/java/TwoPointers.java
@@ -120,4 +120,39 @@ public class TwoPointers {
         }
         return trappedWater;
     }
+    /**
+     * There is an integer array nums sorted in ascending order (with distinct values).
+     *
+     * Prior to being passed to your function, nums is possibly rotated at an unknown pivot index
+     * k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed).
+     * For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
+     *
+     * Given the array nums after the possible rotation and an integer target, return the index of target
+     * if it is in nums, or -1 if it is not in nums.
+     *
+     * You must write an algorithm with O(log n) runtime complexity.
+     * **/
+    public static int searchInRotatedArray(int[] nums, int target) {
+        int start = 0, end = nums.length-1;
+        while (start <= end) {
+            int mid = start + (end-start)/2;
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] > target) {
+                // left half is monotonic, yet still does not satisfy
+                if (nums[start] <= nums[mid] && nums[start] > target) {
+                    start = mid+1;
+                } else {
+                    end = mid-1;
+                }
+            } else {
+                // right half is monotonic , yet still does not satisfy
+                if (nums[end] >= nums[mid] && nums[end] < target) {
+                    end = mid-1;
+                } else {
+                    start = mid+1;
+                }
+            }
+        }
+        return -1;
+    }
 }
