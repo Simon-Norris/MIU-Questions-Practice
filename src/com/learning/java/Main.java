b56150ffcb9db32a987a03c816a2a8642733a3bb
simon Norris <sumanneupane19982055@gmail.com>
2022-08-22 16:04:23 +0545

    solution for countDigits and isBunker array in java

--- src/com/learning/java/Main.java
+++ src/com/learning/java/Main.java
@@ -5,6 +5,18 @@ public class Main
 {
 
     public static void main(String[] args) {
+        int digit = 6;
+        int n = 333333123;
+        System.out.printf("The number of %s in %s is %s ", digit, n, Solution5.countDigit(n, digit));
+        System.out.println();
+
+        int[] arr ={4, 9, 6, 15, 21};
+        int response = Solution5.isBunkerArray(arr);
+        if (response == 1) {
+            System.out.println("Is Bunker Array?: true");
+        } else{
+            System.out.println("Is Bunker Array?: false");
+        }
 
     }
 }
--- src/com/learning/java/Solution/Solution5.java
+++ src/com/learning/java/Solution/Solution5.java
@@ -1,5 +1,30 @@
 package com.learning.java.Solution;
 
 public class Solution5 {
+    private static int isPrime(int num) {
+        if (num < 2) return 0;
+        for (int i = 2; i <= num/2; i++) {
+            if (num % i == 0) return 0;
+        }
+        return 1;
+    }
 
+    public static int countDigit(int n, int digit) {
+        if (n < 0 || digit < 0) return 0;
+        int count = 0;
+        while (n != 0) {
+            if (n%10 == digit) count++;
+            n /= 10;
+        }
+        return count;
+    }
+
+    public static int isBunkerArray(int[] a) {
+        for (int i = 0; i < a.length-1; i++) {
+           if (a[i] % 2 != 0) {
+               if (isPrime(a[i+1]) == 1) return 1;
+           }
+        }
+        return 0;
+    }
 }
