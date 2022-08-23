877b789f5cbef276667fc40f6e3885f2077951ff
simon Norris <sumanneupane19982055@gmail.com>
2022-08-23 18:24:49 +0545

    solution of isCentered, isDual and arrayOfAllPossibilites in java

--- src/com/learning/java/Main.java
+++ src/com/learning/java/Main.java
@@ -7,6 +7,32 @@ public class Main
     public static void main(String[] args) {
 
 
+        int[] arr =  {2} ;
+        int response = Solution7.isCentered(arr);
+        if (response == 1) {
+            System.out.println("Is Centered Array?: true");
+        } else{
+            System.out.println("Is Centered Array?: false");
+        }
+
+
+        int[] arr1 = {1, 2, 2, 1, 3, 0};
+        int response1 = Solution7.isDual(arr1);
+        if (response1 == 1) {
+            System.out.println("Is Dual Array?: true");
+        } else{
+            System.out.println("Is Dual Array?: false");
+        }
+
+        int[] arr2 = {1, 5, 0, 2, 3, 4};
+        int response2 = Solution7.isAllPossibilities(arr2);
+        if (response2 == 1) {
+            System.out.println("Is an array of all possibilities?: true");
+        } else{
+            System.out.println("Is an array of all possibilities ?: false");
+        }
+
+
     }
 }
 
--- src/com/learning/java/Solution/Solution7.java
+++ src/com/learning/java/Solution/Solution7.java
@@ -2,4 +2,46 @@ package com.learning.java.Solution;
 
 public class Solution7 {
 
+    public static int isCentered(int[] a){
+        if (a == null || a.length % 2 == 0) return 0;
+
+        int centered = a[a.length/2];
+        for (int i = 0; i < a.length; i++) {
+            if (i == a.length/2) continue;
+            if (a[i] <= centered) return 0;
+        }
+        return 1;
+    }
+
+    public static int isDual(int[] a){
+        if (a == null) return 0;
+        if (a.length == 0) return 1;
+        if (a.length % 2 != 0) return 0;
+        int sum = a[0]+a[1];
+        for (int i = 0; i < a.length-1; i=i+2) {
+            if ( a[i] + a[i+1] != sum) return 0;
+        }
+        return 1;
+    }
+
+    public static int isAllPossibilities(int[] a) {
+        if (a == null || a.length == 0) return 0;
+
+        int start = 0;
+        int end = a.length - 1;
+
+        for (int i = start; i < end; i++) {
+            boolean found  = false;
+            for (int j = 0; j < a.length; j++) {
+                if (i == a[j]){
+                    found = true;
+                    break;
+                }
+            }
+            if(!found) return 0;
+        }
+
+        return 1;
+    }
+
 }
