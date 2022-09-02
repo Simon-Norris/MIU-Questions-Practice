8545c249f6bda49fee5edc5572707720b875b3d8
simon Norris <sumanneupane19982055@gmail.com>
2022-09-02 12:07:29 +0545

    varoious solutions

--- src/com/learning/java/Main.java
+++ src/com/learning/java/Main.java
@@ -6,6 +6,28 @@ public class Main
 
     public static void main(String[] args) {
 
+        int[] arr ={1, 2, 3, 4, 5, 6, 7, 8 ,9, 10, 11, 12};
+        int num = 10;
+        int response7 = Solution10.hasNValues(arr, num);
+        if (response7 == 1) {
+            System.out.println("Has N values?: true");
+        } else {
+            System.out.println("Has N values?: false");
+        }
+
+        int n = 6;
+        System.out.println("The next perfect square is: "+Solution10.nextPerfectSquare(n));
+
+        int[] a =  {2, 3, 1, -6, 8, -3, -1, 2};
+        int n3 = 5;
+        System.out.println("The total nUpCount is: "+Solution10.nUpCount(a, n3));
+
+        int start = -10, end = 6;
+        System.out.println("The total prime count from start to end is: "+Solution10.primeCount(start, end));
+
+        int[] arr5 =  {2, 12, 4, 6, 8, 11};
+
+        System.out.println("Is it inertial Array ? 1 means true: 0 means false: "+Solution10.isInertial(arr5));
     }
 }
 
--- src/com/learning/java/Solution/Solution10.java
+++ src/com/learning/java/Solution/Solution10.java
@@ -2,4 +2,95 @@ package com.learning.java.Solution;
 
 public class Solution10 {
 
+    public static int hasNValues(int[] a, int n) {
+        if ( n < 0 || a== null || a.length == 0 || n > a.length) return 0;
+
+        int[] arr = new int[n];
+        int count = 0;
+        int ind = 0;
+        for (int i = 0; i < a.length; i++) {
+            int k = 0;
+           boolean found = false;
+           while (k < arr.length && count < arr.length) {
+               if (a[i] == arr[k]) {
+                   found = true;
+                   break;
+               }
+               k++;
+           }
+           count++;
+           if (count > n) return 0;
+           if (!found) {
+               arr[ind] = a[i];
+               ind++;
+           }
+        }
+
+        return count == n ? 1: 0;
+    }
+
+    public static int nextPerfectSquare(int n) {
+        if (n < 0) return 0;
+        else if (n == 0) return 1;
+        int i=0;
+        while (i*i <= n) i++;
+        return i*i;
+    }
+
+    public static int nUpCount(int[] a, int n) {
+        if (a == null || a.length == 0) return 0;
+
+        int partialSum = 0, count = 0, prevSum = 0;
+
+        for (int i = 0; i < a.length; i++) {
+            partialSum += a[i];
+            if (prevSum <= n && partialSum > n) count++;
+            prevSum = partialSum;
+        }
+        return count;
+    }
+
+    public static int primeCount(int start, int end){
+        if (start > end) return 0;
+        int count = 0;
+        for (int i = start; i <= end; i++) {
+            if (i <= 1) continue;
+            boolean isPrime = true;
+            for (int j = 2; j <= i/2; j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) count++;
+        }
+        return count;
+    }
+
+    public static int isInertial(int[] a){
+        if (a == null || a.length == 0) return 0;
+        boolean oneOdd = false;
+        int maxValue = Integer.MIN_VALUE;
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] % 2 != 0) {
+                oneOdd = true;
+            }
+            if (a[i] > maxValue) maxValue = a[i];
+        }
+        if (!oneOdd || (maxValue % 2 != 0)) return 0;
+
+        boolean flag = true;
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] % 2 != 0) {
+                for (int j = 0; j < a.length; j++) {
+                    if (a[j] % 2 == 0 && a[j] != maxValue) {
+                        if (a[i] < a[j]) flag = false;
+                    }
+                }
+            }
+        }
+
+        if (!flag) return 0;
+        return 1;
+    }
 }
