5277e441f60a94d8a360415dbcb3f8f4ffd7464e
simon Norris <sumanneupane19982055@gmail.com>
2022-09-03 20:40:35 +0545

    solution of solve10 and repsEqual

--- src/com/learning/java/Main.java
+++ src/com/learning/java/Main.java
@@ -6,46 +6,12 @@ public class Main
 
     public static void main(String[] args) {
 
-        int[] arr ={1, 2, 3, 4, 5, 6, 7, 8 ,9, 10, 11, 12};
-        int num = 10;
-        int response7 = Solution10.hasNValues(arr, num);
-        if (response7 == 1) {
-            System.out.println("Has N values?: true");
-        } else {
-            System.out.println("Has N values?: false");
-        }
+        int[] result = Solution11.solve10();
+        System.out.println("The solution of solve10 are x: "+ result[0] +" and y: " +result[1]);
 
-        int n = 6;
-        System.out.println("The next perfect square is: "+Solution10.nextPerfectSquare(n));
-
-        int[] a =  {2, 3, 1, -6, 8, -3, -1, 2};
-        int n3 = 5;
-        System.out.println("The total nUpCount is: "+Solution10.nUpCount(a, n3));
-
-        int start = -10, end = 6;
-        System.out.println("The total prime count from start to end is: "+Solution10.primeCount(start, end));
-
-        int[] arr5 =  {2, 12, 4, 6, 8, 11};
-
-        System.out.println("Is it inertial Array ? 1 means true: 0 means false: "+Solution10.isInertial(arr5));
-
-        int[] arr6 = {9, 0,2,-5, 7};
-        System.out.println("The total count of square pairs is : "+Solution10.countSquarePairs(arr6));
-
-        int num7 = 919;
-        System.out.println("The porcupine number is: "+Solution10.findProcupineNumber(num7));
-
-        int[] arr8 = {7, 22, 11, 34, 17, 52, 26, 13, 40, 20, 10, 5, 16, 8, 4, 2, 1};
-        System.out.println("Is it gutherie sequence? "+Solution10.isGutherieSequence(arr8));
-
-        int[] arr9 = {1, 4, 3, 2, 1, 2, 3, 2};
-        System.out.println("The stanton measure of given array is "+Solution10.stantonMeasure(arr9));
-
-        int[] arr10 = {3, 0, 2, -5, 0} ;
-        System.out.println("The sum factor of given array is "+Solution10.sumFactor(arr10));
-
-        int num11 = 42;
-        System.out.println("The Guthrie index is  "+Solution10.gutherieIndex(num11));
+        int[] arr1 = {0, 3, 2, 0, 5, 3};
+        int n1 = 32053;
+        System.out.println("Is reps equal ? "+Solution11.repsEqual(arr1, n1));
     }
 }
 
---
+++ src/com/learning/java/Solution/Solution11.java
@@ -0,0 +1,44 @@
+package com.learning.java.Solution;
+
+public class Solution11 {
+
+    private static int factorial(int n) {
+        if ( n== 1) return 1;
+        return n * factorial(n-1);
+    }
+
+    public static int[] solve10() {
+        int[] result = new int[2];
+        int tenFactorial = factorial(10);
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++) {
+                if ( factorial(i) + factorial(j) == tenFactorial) {
+                    result[0] = i;
+                    result[1] = j;
+                    return result;
+                }
+            }
+        }
+        return result;
+    }
+
+    public static int repsEqual(int[] a, int n) {
+        if (a == null || a.length == 0) return 0;
+
+        int l =0, r=a.length-1;
+        while (l <= r) {
+            int temp = a[l];
+            a[l] = a[r];
+            a[r] = temp;
+            l++; r--;
+        }
+        int length =0;
+        while (n != 0) {
+            int rem = n % 10;
+            if (length < a.length && rem == a[length]) length++;
+            else return 0;
+            n=n/10;
+        }
+        return 1;
+    }
+}
