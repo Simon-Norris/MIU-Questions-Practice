ea6dfa2f702a748b65ab5be626595f64a195e0ae
simon Norris <sumanneupane19982055@gmail.com>
2022-09-11 16:03:29 +0545

    solution of do integer based rounding, isCubePowerful and  decode array

--- src/com/learning/java/Main.java
+++ src/com/learning/java/Main.java
@@ -6,6 +6,26 @@ public class Main
 
     public static void main(String[] args) {
 
+        int[] a1 = {1,2,3,4,5};
+        int n1 = 2;
+        System.out.println("Before doing n based integer rounding:");
+        for (int i = 0; i < a1.length; i++) {
+            System.out.print(a1[i]+ " ");
+        }
+        System.out.println();
+        Solution14.doIntegerBasedRounding(a1, n1);
+        System.out.println("After doing n based integer rounding:");
+        for (int i = 0; i < a1.length; i++) {
+            System.out.print(a1[i]+ " ");
+        }
+        System.out.println();
+
+
+        int n2= -81;
+        System.out.println("Is cube powerful ? "+Solution14.isCubePowerful(n2));
+
+        int[] a2 = {1, 1};
+        System.out.println("THe decoded number from array is: "+Solution14.decodeArray(a2));
     }
 }
 
--- src/com/learning/java/Solution/Solution14.java
+++ src/com/learning/java/Solution/Solution14.java
@@ -1,4 +1,41 @@
 package com.learning.java.Solution;
 
 public class Solution14 {
+
+    public static void doIntegerBasedRounding(int[] a, int n) {
+        if ( n <= 0) return;
+
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] <= 0) continue;
+            int val = (a[i] / n) * n;
+            if (Math.abs(a[i] - val) < Math.abs(a[i] - val - n)) a[i] = val;
+            else a[i] = val + n;
+        }
+    }
+
+    public static int isCubePowerful (int n) {
+        if ( n <= 0) return 0;
+        int sum = 0, num = n;
+        while (num != 0) {
+            int r = num % 10;
+            sum +=  (r * r * r);
+            num = num / 10;
+        }
+
+        if (sum == n) return 1;
+        return 0;
+    }
+
+    public static int decodeArray (int[] a) {
+        int result =0, mult = 1;
+
+        for (int i = a.length-1; i - 1 >= 0; i--) {
+            int diff = Math.abs(a[i-1] - a[i]);
+            result += diff * mult;
+            mult *= 10;
+        }
+
+        if (a[0] < 0) return -result;
+        return result;
+    }
 }
