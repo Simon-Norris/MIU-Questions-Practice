6e2d6c0b38f973fe49d071eef5a5d2e857f43be1
simon Norris <sumanneupane19982055@gmail.com>
2022-09-28 11:28:35 +0545

    practice of other questions

--- src/com/learning/java/Main.java
+++ src/com/learning/java/Main.java
@@ -17,6 +17,36 @@ public class Main
 
         int seconds = 7320;
         System.out.println("The converted second int HMS is "+Miscellaneous.secondToHMS(seconds));
+
+        int[] arr = {20, 30, 40};
+        int n = 1;
+        System.out.printf("Before rotating given array %s place",n);
+        System.out.println();
+        for (int j : arr) {
+            System.out.print(j +" ");
+        }
+        System.out.printf("\nAfter rotating given array %s place",n);
+        Miscellaneous.rotateArray(arr, n);
+        System.out.println();
+        for (int j : arr) {
+            System.out.print(j+" ");
+        }
+
+        int[] first = {1, 3, -5, 4};
+        int[] second = {1, 4, -5, -2};
+        System.out.println("\nAfter multiplying two arrays");
+        int[] response = Miscellaneous.multiplyTwoArr(first, second);
+        for (int i = 0; i < response.length; i++) {
+            System.out.print(response[i]+" ");
+        }
+
+        int[] a = {3,4,5,6,7,8,9,10,11,12,13,14,115};
+        int[] b ={1,2,3,4,5,6,7,24,67,89,114,170};
+        System.out.println("\nAfter sorting");
+        int[] res = Miscellaneous.mergeSortedArrays(a, b);
+        for (int re : res) {
+            System.out.print(re + " ");
+        }
     }
 }
 
--- src/com/learning/java/Solution/Miscellaneous.java
+++ src/com/learning/java/Solution/Miscellaneous.java
@@ -1,5 +1,7 @@
 package com.learning.java.Solution;
 
+import java.util.concurrent.TimeUnit;
+
 public class Miscellaneous {
 
     public static int addBinary(int first, int second) {
@@ -71,5 +73,67 @@ public class Miscellaneous {
         int minutes = rem / 60;
         int sec = rem % 60;
         return (hour+"H:"+minutes+"M:"+sec+"S");
+    }
+
+    //{40, 50, 60, 70, 80} -> {60, 70, 80, 40, 50}
+    public static void rotateArray(int[] a, int n){
+        if (a == null || a.length <= 1) return;
+
+        for (int i = 0; i < n; i++) {
+            int temp = a[0];
+            int j = 0;
+            while (j+1 < a.length){
+                a[j] = a[j+1];
+                j++;
+            }
+            a[j] = temp;
+        }
+    }
+
+    public static int[] multiplyTwoArr(int[] first, int[] second){
+        if (first == null || second == null || first.length == 0 || second.length == 0) return new int[]{};
+
+        int max = Math.max(first.length, second.length);
+        int[] arr = new int[max];
+        for (int i = 0, j=0; i < first.length && j < second.length; i++,j++) {
+            arr[i] = first[i] * second[j];
+        }
+        return arr;
+    }
+
+    public static int[] mergeSortedArrays(int[] first, int[] second){
+        if ((first == null && second == null) || (first.length == 0 && second.length == 0)) return new int[]{};
+
+        int length = first.length + second.length;
+        int[] arr = new int[length];
+        int k = 0;
+
+        for (int i = 0; i < first.length; i++) {
+            arr[k++] = first[i];
+        }
+        for (int i = 0; i < second.length; i++) {
+            arr[k++] = second[i];
+        }
+        insertionSort(arr);
+        return arr;
+    }
+
+    private static void insertionSort(int[] arr) {
+        if (arr == null || arr.length == 0) return;
+
+        long start = System.nanoTime();
+        System.out.println("\nSorting Started: "+start);
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i-1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = key;
+        }
+        long finished = System.nanoTime()-start;
+        int sec1 = (int) TimeUnit.SECONDS.convert(finished, TimeUnit.NANOSECONDS);
+        System.out.println("\nSorting Finished: "+sec1);
     }
 }
