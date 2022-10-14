f12dd669653816c0b48ee58ff15aa965bf354f87
simon Norris <sumanneupane19982055@gmail.com>
2022-10-14 11:15:43 +0545

    print star pattern

--- src/com/learning/java/Main.java
+++ src/com/learning/java/Main.java
@@ -23,6 +23,27 @@ public class Main
         int[] arr = {-1,0,1,2,-1,-4};
         List<List<Integer>> list = TwoPointers.tripletZeroSum(arr);
         System.out.println(list);
+
+        /*
+        * print this pattern
+              #
+             ###
+            #####
+           #######
+          #########
+        * */
+        int n = 5;
+        for (int i= 1; i<= n; i++){
+
+            for (int j = 0; j < n-i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 0; j < (2*i - 1); j++) {
+                System.out.print("#");
+            }
+            System.out.println();
+        }
     }
 }
 
