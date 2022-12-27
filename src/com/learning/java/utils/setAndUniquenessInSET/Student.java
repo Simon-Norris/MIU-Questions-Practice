d8d0729011fb926bea639870d47fa84bca0b5502
simon Norris <sumanneupane19982055@gmail.com>
2022-12-27 16:37:11 +0545

    init hashcode example for hashcode() and equals()

---
+++ src/com/learning/java/utils/setAndUniquenessInSET/Student.java
@@ -0,0 +1,31 @@
+package com.learning.java.utils.setAndUniquenessInSET;
+
+public class Student {
+    private String name;
+    private int age;
+    private int yearOfPassing;
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public int getAge(){
+        return this.age;
+    }
+
+    public void setAge(int age){
+        this.age = age;
+    }
+
+    public int getYearOfPassing(){
+        return this.yearOfPassing;
+    }
+
+    public void setYearOfPassing(int year){
+        this.yearOfPassing = year;
+    }
+}
