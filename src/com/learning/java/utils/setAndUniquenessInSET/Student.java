a15476cc7e5698ef83508889599bb7ee63287d3c
simon Norris <sumanneupane19982055@gmail.com>
2022-12-27 16:48:48 +0545

    hashset after implementation of equals() and hashCode()

--- src/com/learning/java/utils/setAndUniquenessInSET/Student.java
+++ src/com/learning/java/utils/setAndUniquenessInSET/Student.java
@@ -2,8 +2,8 @@ package com.learning.java.utils.setAndUniquenessInSET;
 
 public class Student {
     private String name;
-    private int age;
-    private int yearOfPassing;
+    private Integer age;
+    private Integer yearOfPassing;
 
     public String getName(){
         return this.name;
@@ -27,5 +27,18 @@ public class Student {
 
     public void setYearOfPassing(int year){
         this.yearOfPassing = year;
+    }
+
+    @Override
+    public boolean equals(Object o){
+        Student m = (Student) o;
+        return m.name.equals(this.name) &&
+                m.age.equals(this.age) &&
+                m.yearOfPassing.equals(this.yearOfPassing);
+    }
+
+    @Override
+    public int hashCode(){
+        return this.name.hashCode() + this.age.hashCode() + this.yearOfPassing.hashCode();
     }
 }
